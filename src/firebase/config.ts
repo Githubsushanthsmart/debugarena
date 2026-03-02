9fdd1dcc69de7a053afbfde5fd27e29890537e27
AKUTHOTA SUSHANTH <smartysushanth67@gmail.com>
2026-03-02 07:25:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5723173173-789b6",
+  "appId": "1:732360421958:web:921a3d55eab05b16d35047",
+  "apiKey": "AIzaSyBebn02ezczJRTwihFSmiuHfKAOuqLQtcg",
+  "authDomain": "studio-5723173173-789b6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "732360421958"
+};
